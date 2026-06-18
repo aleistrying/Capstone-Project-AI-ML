b4f7520bfb9bfff2272758686867742c1fb44c73
ingepsicomaster <carlos.graniel.manrique@gmail.com>
2026-06-18 20:32:33 +0000

    movieId added to recommendations

--- src/metrics/test.py
+++ src/metrics/test.py
@@ -1,6 +1,4 @@
-# Then your import would work as:
-import sys
-sys.path.append('/Workspace/Users/carlos.graniel.manrique@gmail.com/Capstone-Project-AI-ML/notebooks')
-from API_Simulation import test_cineassist_pipeline
-
-#test=test_cineassist_pipeline()
+def accuracy(recommendations):
+    print(recommendations)
+    return
+    
